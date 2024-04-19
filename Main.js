0f1de44c0b21b5ee0f0dec01330dc24f369e69c5
DRSPACEMAN94 <159204742+DRSPACEMAN94@users.noreply.github.com>
2024-04-18 23:44:08 -0700

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,8 +1,8 @@
 // Import Three.js from unpkg
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.150.0/build/three.module.js';
 
 // Import GLTFLoader from unpkg
-import { GLTFLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.150.0/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
